7ceb667926cf6d0ef6776fe31b2a3addb40488b7
nelsonadrian1975 <100170433+nelsonadrian1975@users.noreply.github.com>
2026-05-29 12:09:04 -0500

    Agregar cargador de scripts

---
+++ app.js
@@ -0,0 +1,9 @@
+[
+  "src/app-1-core.js",
+  "src/app-2-map.js",
+  "src/app-3-forms.js",
+  "src/app-4-document.js",
+  "src/app-5-init.js"
+].forEach((src) => {
+  document.write(`<script src="${src}"></script>`);
+});
